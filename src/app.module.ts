a906c1cfb5612a6921f9b338aca076c1a3e47898
Asisipho Daraza <daraza@thedigitalacademy.co.za>
2021-09-15 11:39:25 +0200

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
-	imports: [
-		// MongooseModule.forRoot("mongodb+srv://nolan__kgotso:Nolan123@phonebook.gu113.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true, }),
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
-		PhonebookModule,
-	],
-	controllers: [],
-	providers: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://asisipho:asisipho@phonebook.o9chy.mongodb.net/PhonebookDatabase?retryWrites=true&w=majority',
+      { autoCreate: true },
+    ),
+    // MongooseModule.forRoot('mongodb://localhost:27017/PHONEBOOK_DB', {
+    //   autoCreate: true,
+    // }),
+    PhonebookModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
